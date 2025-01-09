4c5a1dc7ff92ecc4ec828b9687e8b5a65a43b3e5
cschu9 <catherineschuch@CSchuLaptop>
2025-01-09 07:37:38 -0600

    Implimented sparkcan and added sparkmax specific code for 1 motor and 1 actuator to drivetrain.cpp and scoop.cpp

--- src/control_pkg/src/drivetrain.cpp
+++ src/control_pkg/src/drivetrain.cpp
@@ -1,5 +1,6 @@
 #include "rclcpp/rclcpp.hpp"
 #include "interfaces_pkg/msg/duty_cycle_output.hpp"
+#include "SparkMax.hpp"
 
 class DrivetrainNode : public rclcpp::Node
 {
@@ -8,12 +9,37 @@ class DrivetrainNode : public rclcpp::Node
 		: Node("drivetrain") {
 			duty_subscriber = this->create_subscription<interfaces_pkg::msg::DutyCycleOutput>(
       "control/duty_cycle", 10, std::bind(&DrivetrainNode::duty_cycle_control, this, std::placeholders::_1));
+      
+      SparkMax left_drive("can0", 1);
+      SparkMax right_drive("can0", 2);
+      //Creates SparkMax Objects
+      
+      left_drive.SetIdleMode(IdleMode::kCoast);
+      left_drive.SetMotorType(MotorType::kBrushless);
+      left_drive.BurnFlash();
+      //Burn left drive motor
+      
+      right_drive.SetIdleMode(IdleMode::kCoast);
+      right_drive.SetMotorType(MotorType::kBrushless);
+      right_drive.BurnFlash();
+      //Burn right drive motor
+      
 		}
 		
 	private:
 		void duty_cycle_control(const interfaces_pkg::msg::DutyCycleOutput & msg) const {
 			
 			RCLCPP_INFO(this->get_logger(), "%f %f", msg.left_drive_output, msg.right_drive_output);
+			
+			SparkMax left_drive("can0", 1);
+      			SparkMax right_drive("can0", 2);
+      			//Creates SparkMax Objects
+			
+			SparkMax::Heartbeat();
+			left_drive.SetDutyCycle(msg.left_drive_output);
+			right_drive.SetDutyCycle(msg.right_drive_output);
+			//send duty cycle output to left and right drive
+			
 		}
 		
 		rclcpp::Subscription<interfaces_pkg::msg::DutyCycleOutput>::SharedPtr duty_subscriber;
--- src/control_pkg/src/scoop.cpp
+++ src/control_pkg/src/scoop.cpp
@@ -1,19 +1,34 @@
 #include "rclcpp/rclcpp.hpp"
 #include "interfaces_pkg/msg/duty_cycle_output.hpp"
+#include "SparkMax.hpp"
 
 class ScoopNode : public rclcpp::Node
 {
 	public:
 		ScoopNode()
-		: Node("scoop") {
+		: Node("scoop"){
 			duty_subscriber = this->create_subscription<interfaces_pkg::msg::DutyCycleOutput>(
       "control/duty_cycle", 10, std::bind(&ScoopNode::duty_cycle_control, this, std::placeholders::_1));
+      
+      SparkMax left_lift("can0", 3);
+      //Creates left_lift object
+      
+      left_lift.SetIdleMode(IdleMode::kBrake);
+      left_lift.SetMotorType(MotorType::kBrushed);
+      left_lift.SetInverted(true);  //TO-DO: test out actuators to see if they are inverted or not
+      left_lift.BurnFlash();
+      
 		}
 		
 	private:
 		void duty_cycle_control(const interfaces_pkg::msg::DutyCycleOutput & msg) const {
 		
 			RCLCPP_INFO(this->get_logger(), "%f %f %f %f", msg.left_lift_output, msg.right_lift_output, msg.tilt_output, msg.vibrator_output);
+			
+			SparkMax left_lift("can0", 3);
+     			//Creates left_lift object
+			SparkMax::Heartbeat();
+      			left_lift.SetDutyCycle(msg.left_lift_output);
 		}
 		
 		rclcpp::Subscription<interfaces_pkg::msg::DutyCycleOutput>::SharedPtr duty_subscriber;
